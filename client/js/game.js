a0cf69611f9b4b342081cd391fca148b94dcbec5
rkanderson <ryananderson@Ryans-MacBook-Pro-3.local>
2017-02-08 17:00:49 -0800

    Message changed

--- client/js/game.js
+++ client/js/game.js
@@ -18,9 +18,9 @@ Game.prototype.handleGraphics = function(gfx) {
 
   gfx.fillStyle = '#2ecc71';
   gfx.strokeStyle = '#27ae60';
-  gfx.font = 'bold 50px Verdana';
+  gfx.font = 'bold 30px Monospace';
   gfx.textAlign = 'center';
   gfx.lineWidth = 2;
-  gfx.fillText('I can instant message through git commits!', screenWidth / 2, screenHeight / 2);
+  gfx.fillText('Nope it takes like 20 seconds for the server to apply the changes. How much space does it take up if I write a really long sentence. Hmmmmmm', screenWidth / 2, screenHeight / 2);
 //  gfx.strokeText('Now playing...', screenWidth / 2, screenHeight / 2);
 }
